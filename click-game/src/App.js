319b7e95d0f72b2cd92a1b254b216d85c9bb4a58
John Falk <jrfalk@outlook.com>
2019-09-28 19:43:19 -0400

    Created initial file layout

---
+++ click-game/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Card from "./components/Cards";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Cards from "./cards.json";
+
+class App extends Component {
+}
+
+export default App;
