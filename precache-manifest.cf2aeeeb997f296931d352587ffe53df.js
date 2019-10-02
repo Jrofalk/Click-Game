93bfcd31b91b90ebabfdb13da7183ca54607ead3
John Falk <jrfalk@outlook.com>
2019-10-01 21:28:34 -0400

    Updates

---
+++ precache-manifest.cf2aeeeb997f296931d352587ffe53df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026f0addc5628fdb2943b9cbb79f8fb5",
+    "url": "/Click-Game/index.html"
+  },
+  {
+    "revision": "6c540d22c776c2baf786",
+    "url": "/Click-Game/static/css/main.65a56feb.chunk.css"
+  },
+  {
+    "revision": "54e96837d66e25975c76",
+    "url": "/Click-Game/static/js/2.636d69d3.chunk.js"
+  },
+  {
+    "revision": "6c540d22c776c2baf786",
+    "url": "/Click-Game/static/js/main.ea7ae06f.chunk.js"
+  },
+  {
+    "revision": "99409d0f31c44d99d683",
+    "url": "/Click-Game/static/js/runtime-main.84d5dc95.js"
+  }
+]);
